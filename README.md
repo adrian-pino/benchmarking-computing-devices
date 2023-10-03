466bc6c858169bde5d40b38f77e0bc78104fc5c5
Pino, Adrián <adrian.pino@i2cat.net>
2023-10-03 13:48:01 +0200

    Solving conflicts

---
+++ README.md
@@ -0,0 +1,38 @@
+# Project Name
+
+Benchmarking Computing Devices (network, processing time)
+
+## Requirements
+
+Before you begin, ensure you have met the following requirements:
+
+- [Python 3](https://www.python.org/downloads/)
+- [venv (Python Virtual Environment)](https://docs.python.org/3/library/venv.html)
+
+## Setup
+
+Follow these steps to set up your project environment:
+
+1. Create a virtual environment named "xgain" using `venv`:
+
+    ```bash
+    python3 -m venv venv-xgain
+    ```
+
+2. Activate the virtual environment:
+
+    On Linux:
+
+    ```bash
+    source venv-xgain/bin/activate
+    ```
+
+3. You are now in the virtual environment and can proceed with your project setup and execution.
+
+## Usage
+
+Provide instructions on how to use your project or run any scripts.
+
+## License
+
+This project is licensed under the XYZ License - see the [LICENSE](LICENSE) file for details.
--- scripts/processing_time/processing_time.py
+++ scripts/processing_time/processing_time.py
@@ -5,8 +5,13 @@ import json
 import configparser
 from pymongo import MongoClient
 
+# Define the database name and collection name
+database_name = "benchmarking-computing"
+collection_name = "farms"
+
 # Function to insert data and measure
 def insert_data_and_measure(farm_data, mongo_uri, num_repetitions):
+    print(f"Connecting to MongoDB at {mongo_uri} on device {device_name}")  # Print the connection information
     client = MongoClient(mongo_uri)
     db = client[database_name]
     collection = db[collection_name]
@@ -41,9 +46,9 @@ def read_sample_data(filename):
 def read_config(filename):
     config = configparser.ConfigParser()
     config.read(filename)
-    repetitions = int(config['General']['repetitions'])
-    mongodb_port = int(config['General']['mongodb_port'])  # Read MongoDB port setting
-    results_folder = config['General']['results_folder']  # Read results folder setting
+    repetitions = int(config['general']['repetitions'])
+    mongodb_port = int(config['general']['mongodb_port'])  # Read MongoDB port setting
+    results_folder = config['general']['results_folder']  # Read results folder setting
     
     far_edge_ips = {}
     for key, value in config['far-edge'].items():
@@ -57,35 +62,62 @@ def read_config(filename):
 
 if __name__ == "__main__":
     try:
+        script_name = os.path.basename(__file__)
+        print(f"Executing script: {script_name}")
+        print("**************************************************************************************************************")
+        print("This script inserts data into different MongoDBs databases placed on different devices and measures performance.")
+
+
         # Read configuration from the config.cfg file
         repetitions, mongodb_port, results_folder, far_edge_ips, edge_ips = read_config("config.cfg")
-        
+
         # Read sample data from the JSON file
         farm_data = read_sample_data("sample_agriculture_data.json")
-        
-        # Create results directory if it doesn't exist
-        os.makedirs(results_folder, exist_ok=True)
+
+        # Create a dictionary to store results
+        results_dict = {}
+
+        print(f"Results will be stored in the folder: {results_folder}")
+        print("Results include success rates and average insertion times for different devices.")
+        print("**************************************************************************************************************")
 
         for farm in farm_data:
             farm_name = farm["farm_name"]
-            
+
+            # Create sub-dictionaries for each farm
+            farm_results = {"far_edge": {}, "edge": {}}
+
             for device_name, device_ip in far_edge_ips.items():
                 mongo_uri = f"mongodb://{device_ip}:{mongodb_port}"  # Include MongoDB port
                 success_count, avg_time = insert_data_and_measure(farm_data, mongo_uri, repetitions)
-                
-                # Write results to a file in the specified results folder
-                result_file = os.path.join(results_folder, f"{farm_name}_{device_name}_results.txt")
-                with open(result_file, "w") as file:
-                    file.write(f"Farm Name: {farm_name}\n")
-                    file.write(f"Device Name: {device_name}\n")
-                    file.write(f"Device IP: {device_ip}\n")
-                    file.write(f"Success Rate: {success_count/repetitions*100:.2f}%\n")
-                    file.write(f"Average Insertion Time: {avg_time:.5f} seconds\n")
+
+                # Store results for far-edge devices
+                farm_results["far_edge"][device_name] = {
+                    "Device IP": device_ip,
+                    "Success Rate": success_count / repetitions * 100,
+                    "Average Insertion Time": avg_time,
+                }
 
             for device_name, device_ip in edge_ips.items():
                 mongo_uri = f"mongodb://{device_ip}:{mongodb_port}"  # Include MongoDB port
                 success_count, avg_time = insert_data_and_measure(farm_data, mongo_uri, repetitions)
-                
-                # Write results to a file in the specified results folder
-                processing_time_results.txt
 
+                # Store results for edge devices
+                farm_results["edge"][device_name] = {
+                    "Device IP": device_ip,
+                    "Success Rate": success_count / repetitions * 100,
+                    "Average Insertion Time": avg_time,
+                }
+
+            # Store farm-specific results in the main results dictionary
+            results_dict[farm_name] = farm_results
+
+        # Save the results dictionary as a JSON file
+        results_file = os.path.join(results_folder, "results.json")
+        with open(results_file, "w") as json_file:
+            json.dump(results_dict, json_file, indent=4)
+
+        print("Results saved to results.json")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
