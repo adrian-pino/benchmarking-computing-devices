ddf8f55dc3ae292c47491fe1980d43ae34069fc7
adrian.pino@i2cat.net <adrian.pino@i2cat.net>
2023-10-11 14:29:49 +0200

    network throughput bug fixing. Add support of Gbps.

--- scripts/network_throughput/draw_figure.py
+++ scripts/network_throughput/draw_figure.py
@@ -5,17 +5,23 @@ def parse_throughput(filename):
     with open(filename, 'r') as file:
         content = file.read()
 
-    # Extract the device name and their throughput results
     device_pattern = re.compile(r"Device Name: (.+?)\nDevice IP: .+?\n(.*?)\niperf Done\.", re.DOTALL)
-    throughput_pattern = re.compile(r"\[\s+\d\]\s+\d+\.\d{2}-\d+\.\d{2}\s+sec\s+\d+\sMBytes\s+(\d{3,4})\sMbits/sec")
+    throughput_pattern = re.compile(r"\[\s+\d\]\s+\d+\.\d{2}-\d+\.\d{2}\s+sec\s+\d+(\.\d+)?\s([G|M])Bytes\s+(\d+(\.\d+)?)\s([G|M])bits/sec")
 
     results = {}
 
     for match in device_pattern.findall(content):
         device_name, throughput_data = match
-        throughputs = [float(throughput) for throughput in throughput_pattern.findall(throughput_data)]
-        # Assuming you want to average the throughputs for each device's tests:
-        avg_throughput = sum(throughputs) / len(throughputs)
+        throughputs = []
+        
+        for t_match in throughput_pattern.findall(throughput_data):
+            value, _, unit = float(t_match[2]), t_match[3], t_match[4]
+            # Convert to Mbits/sec if necessary
+            if unit == "G":
+                value *= 1000
+            throughputs.append(value)
+
+        avg_throughput = sum(throughputs) / len(throughputs) if throughputs else 0
         results[device_name] = avg_throughput
 
     return results
